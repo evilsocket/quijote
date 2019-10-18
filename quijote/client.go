86669e7720e750359bae41f0f390fe994a024e80
Simone Margaritelli <evilsocket@gmail.com>
2019-10-18 14:53:25 +0200

    misc: small fix or general refactoring i did not bother commenting

--- quijote/client.go
+++ quijote/client.go
@@ -7,6 +7,7 @@ import (
 	"time"
 )
 
+// Client is an object that holds the needed information about the request client.
 type Client struct {
 	// on some archs stuff used with the atomic package must be aligned
 	// as first element otherwise unaligned memory exceptions might happen
@@ -19,6 +20,7 @@ type Client struct {
 	BannedAt   time.Time
 }
 
+// ParseClient returns a Client object parsed from the provided request.
 func ParseClient(r *http.Request) Client {
 	addr := strings.Split(r.RemoteAddr, ":")[0]
 	cli := Client{
@@ -40,6 +42,8 @@ func ParseClient(r *http.Request) Client {
 	return cli
 }
 
+// String returns a string representation of the Client object indicating its real IP
+// if different than its remote address.
 func (cli Client) String() string {
 	ip := cli.RemoteAddr
 	for _, value := range cli.Addresses {
